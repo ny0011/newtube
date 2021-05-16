d6b635b603aadedf77ccb5ffdacecaaef5a60bd8
ny0011 <nayun011@gmail.com>
2021-05-16 23:10:03 +0900

    my first node.js server 🚀

---
+++ STUDY.md
@@ -0,0 +1,16 @@
+## node.js
+- javascript가 웹 브라우저 말고도 실행할 수 있게 해주는 v8 기반 runtime
+- JavaScript runtime built on Chrome's V8 JavaScript engine
+    - v8 : Google open source javascript and webassembly engine, C++로 작성됨
+
+## npm
+- node package manager
+- node.js에서 사용할 수 있는 패키지를 다운로드 할 수 있는 곳
+
+## babel
+- javascript compiler, toolchain
+- ECMAScript 2015+ 최신 코드를 예전 코드로 바꿔줌
+- 웹 브라우저 호환성을 높여줌
+
+## express
+- node.js의 간단한 web app framework
---
+++ src/server.js
@@ -0,0 +1,19 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = (req, res) => {
+    return res.send("Hello 🎈")
+}
+const handleLogin = (req, res) => {
+    return res.send("Login here 🎇")
+}
+app.get("/", handleHome)
+app.get("/login", handleLogin)
+
+const handleListening = () =>
+    console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+
+app.listen(PORT, handleListening);
