c3ec95968fe0795f7545c44064ea41bc737b6ff5
ny0011 <nayun011@gmail.com>
2023-06-08 11:11:54 +0900

    Router의 URL에서 정규식 사용

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,5 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => res.send("Watch");
+export const see = (req, res) => res.send(`Watch ${req.params.id}`);
 export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
